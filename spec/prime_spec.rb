dfc2746bf9c4457362cf0c7f5f9ba5375610d642
Tommi Cox <tommicox@gmail.com>
2019-03-04 02:25:02 +0000

    Automatically backed up by Learn

--- spec/prime_spec.rb
+++ spec/prime_spec.rb
@@ -20,8 +20,8 @@ describe "prime?" do
   end
 end
 
-def prime?(num)
-  if num < 0 or num == 0 or num 1
+def prime(num)
+  if num < 0 or num == 0 or num == 1
   return false
 else
   (2..num-1).to_a.all? do |possible_factor| num % possible_factor != 0
