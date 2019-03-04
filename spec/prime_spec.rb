5b5b01e036832267235e9acab24f3586a0a634b6
Tommi Cox <tommicox@gmail.com>
2019-03-04 02:26:23 +0000

    Done.

--- spec/prime_spec.rb
+++ spec/prime_spec.rb
@@ -19,3 +19,13 @@ describe "prime?" do
     expect(prime?(101013)).to be(false)
   end
 end
+
+def prime(num)
+  if num < 0 or num == 0 or num == 1
+  return false
+else
+  (2..num-1).to_a.all? do |possible_factor| 
+    num % possible_factor != 0
+  end
+end
+
