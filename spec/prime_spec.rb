9d18a720d3539896edaf8527690832be728ea7c6
Tommi Cox <tommicox@gmail.com>
2019-03-04 02:25:52 +0000

    Automatically backed up by Learn

--- spec/prime_spec.rb
+++ spec/prime_spec.rb
@@ -24,7 +24,8 @@ def prime(num)
   if num < 0 or num == 0 or num == 1
   return false
 else
-  (2..num-1).to_a.all? do |possible_factor| num % possible_factor != 0
+  (2..num-1).to_a.all? do |possible_factor| 
+    num % possible_factor != 0
   end
 end
 
